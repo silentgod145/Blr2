5daa6900c29bff9fc3d522fe0b1478c2148c175d
BIGDOTLOCK <97359342+B3X2@users.noreply.github.com>
2022-09-07 18:42:41 -0400

    Create .lua

---
+++ .lua
@@ -0,0 +1,15 @@
+NotifyLib.prompt('Tier - NotifyLib is MoonGods not ours', 'loading...', 5)
+   NotifyLib.prompt('Tier - NotifyLib is MoonGods not ours','loading...',4)
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Blr2.lua", "Synapse")
+
+local speed = Window:NewTab("DotLock")
+local rs = speed:NewSection("blr2 Dot")
+rs:NewButton("DotLock", "Legit", function()
+  
+end)
+rs:NewSlider("Hitchance ", "HitChance", 100, 0.2 , 2.4 ,  0, function() 
+end)
+rs:NewKeybind("Toggle UI", "KeybindInfo", Enum.KeyCode.v , function()
+	Library.ToggleUI()
+end)
