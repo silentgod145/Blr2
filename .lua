a514c050929238975541f6acf897ec501f39509c
BIGDOTLOCK <97359342+B3X2@users.noreply.github.com>
2022-09-07 18:47:33 -0400

    Update .lua

--- .lua
+++ .lua
@@ -7,9 +7,10 @@ local Window = Library.CreateLib("Blr2.lua", "Synapse")
 local speed = Window:NewTab("DotLock")
 local rs = speed:NewSection("blr2 Dot")
 rs:NewButton("DotLock", "Legit", function()
-  
+ loadstring(game:HttpGet("https://pastebin.com/raw/JZAkC1Vh"))() 
 end)
 rs:NewSlider("Hitchance ", "HitChance", 100, 0.2 , 2.4 ,  0, function() 
+hit.chance.Humanoid.Hit.HumanoidBody = true
 end)
 rs:NewKeybind("Toggle UI", "KeybindInfo", Enum.KeyCode.v , function()
 	Library.ToggleUI()
